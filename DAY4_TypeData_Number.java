3c45120d7eff7db08e5e69f0418327890baca9bf
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 07:30:19 +0800

    DAY4
    Type data number terbagi menjadi beberapa dengan fungsi untuk menyimpan nilai yang besarnya berbeda

---
+++ DAY4_TypeData_Number.java
@@ -0,0 +1,11 @@
+public class TypeDaataNumber {
+  public static void main(String[] args) {
+    byte iniByte = 150;
+    short iniShort = 6000;
+    int iniInt = 10000000;
+    long iniLong = 1000000000;
+
+    float iniFloat = 20.10F;
+    double iniDouble = 30.50;
+  }
+}
