cfbcd02a06e8064ebc34bf9bc83a82b15a67870b
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 07:04:42 +0800

    Day 2
    Komentar digunakan untuk memberikan penjelasan pada kode program. Ada dua jenis komentar di Java, yaitu komentar satu baris dan komentar multi baris,komentar tidak akan ditampilkan

---
+++ DAY2_Komentar.java
@@ -0,0 +1,12 @@
+// Ini adalah komentar satu baris,
+
+/*
+ini komentar yang mencakup banyak garis
+komentar tidak akan ditampilkan saat deksekusi
+fungsinya adalah untuk menandai perintah agar tidak lupa jika code sudah banyak
+*/
+public class comentar {
+  public static void main(string[] args) {
+    System.out.println("Hello Jankrik");
+  }
+}
