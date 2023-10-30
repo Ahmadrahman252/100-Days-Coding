234f06dd562f4182d42b10d729b9e7e5d31de44e
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 07:14:49 +0800

    Day3
    beeberapa variabel umum

---
+++ DAY3_Variabel_umum.java
@@ -0,0 +1,19 @@
+public class variabel_umum_java {
+    public static void main(String[] args) {
+        // Variabel umum
+        int umur = 18; //bilangan bulat (integer)
+        double  Tinggi = 176.2; //bilangan pecahan (floating-point)
+        String nama = "Ahmad Rahman"; //teks
+        boolean Ganteng = false; //hanya berisi true or false
+        char jk = 'L';
+
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(tinggi);
+        System.out.println(ganteng);
+        System.out.println(jk);
+    }
+}
+
+
+    
