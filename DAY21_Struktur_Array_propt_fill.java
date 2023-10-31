45d45231c0a92a9053be82cec987db70d234358b
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:28:10 +0800

    DAY21
    Metode fill digunakan untuk mengisi semua elemen dalam sebuah array dengan nilai

---
+++ DAY21_Struktur_Array_propt_fill.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Arrays;
+public class struktur_array_filled {
+    public static void main(String[] args) {
+        
+        int[] aku = new int[20];
+        int nilai = 1001919;
+        Arrays.fill(aku, nilai);
+        System.out.println("Isi Array: " + Arrays.toString(aku));
+    }
+}
+
