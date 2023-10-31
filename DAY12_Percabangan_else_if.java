7e194b32a1f1fef1e679bd6bced09e7da95b96e4
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 11:53:29 +0800

    DAY12
    Sama Saja Namun Dengan Pilihan yang lebih banyak

---
+++ DAY12_Percabangan_else_if.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class percabangan_if_else {
+    public static void main(String[] args) {
+        int nilai = 81;
+        if (nilai >= 90) {
+            System.out.println("Nilai : A");
+        }
+         else if (nilai >= 75) {
+            System.out.println("Nilai : B");
+        }
+         else if (nilai >= 65) {
+            System.out.println("Nilai : C");
+        }
+         else {
+            System.out.println("Nilai : D");
+        }
+    }
+}
