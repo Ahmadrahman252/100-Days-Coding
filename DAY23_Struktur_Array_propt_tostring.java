1adec90045a93f275ae1da1dcd3a83dbaf0399b9
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:32:43 +0800

    DAY23
    Digunakan untuk mengonversi objek menjadi representasi string

---
+++ DAY23_Struktur_Array_propt_tostring.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Arrays;
+public class Array_tostring {
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5};
+        String arrayString = Arrays.toString(intArray);
+        System.out.println(arrayString);
+    }
+}
+
