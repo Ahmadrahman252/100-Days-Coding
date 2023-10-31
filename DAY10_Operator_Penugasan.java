5cbb23bd9848defaebd22c4d0de2554a9cea0a3d
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 11:44:50 +0800

    DAY10
    Operator penugasan adalah operator yang digunakan untuk mengisi atau mengubah nilai variabel, menggunakan tanda =

---
+++ DAY10_Operator_Penugasan.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Operator_pennugasan {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 62;
+        int c =  42;
+        int d = 12;
+        int e = 27;
+
+        a += 2; 
+        b *= 3; 
+        c /= 2;
+        d -= 5;
+        e %= 8;
+
+        System.out.println(" a setelah penugasan: " + a);
+        System.out.println("b setelah penugasan: " + b);
+        System.out.println("c setelah penugasan: " + c);
+        System.out.println(" d setelah penugasan: " + d);
+        System.out.println("e setelah penugasan: " + e);
+        
+    }
+}
