3190e71ad8db37360851a91fb459d1561e56d7a6
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:21:13 +0800

    DAY18
    Array sorting akanmengurutkan elemen dalam sebuah array berdasarkan nilai mereka.

---
+++ DAY18_Array_Propt_Sort_Asc.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Arrays;
+public class struktur_array_sort {
+    public static void main(String[] args) {
+        int[] nilai = {3,7,12,10, 5, 8, 9, 1,80,2,2839920,-4};
+        Arrays.sort(nilai);
+        System.out.println("Array setelah diurutkan: " + Arrays.toString(nilai));
+    }
+}
+
