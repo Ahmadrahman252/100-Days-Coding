0d93d29c5fe0468d1e136d3fd3877e71725fd7b4
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:18:50 +0800

    DAY17
    array untuk menyimpan beberapa nilai dalam satu variabel

---
+++ DAY17_Array.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class array {
+    public static void main(String[] args) {
+        int[] angka = new int[5]; // Membuat array integer dengan 5 elemen
+        angka[0] = 10;
+        angka[1] = 20;
+        angka[2] = 30;
+        int nilai = angka[2]; // Akan menghasilkan 30
+        System.out.println("Nilai ketiga Dalam Array Adalah:"+nilai);
+    }
+}
