0add8de38da0463fda69597664306353790fc34b
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:02:53 +0800

    DAY14
    for digunakan  untuk menjalankan  kode secara berulang dengan mengatur jumlah iterasi.

---
+++ DAY14_Perulangan_For.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class perulangan {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 100; a++) {
+            System.out.println(a);
+        }
+    }
+}
+
