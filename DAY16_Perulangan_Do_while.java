1905196fbc8382b7cbc08bab60a8a96cf01e7c79
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:13:51 +0800

    DAY16
    Pernyataan do-while Mirip dengan  while. Namun, perbedaannya terletak pada urutan evaluasi kondisi.  kode akan dijalankan satu kali,  jika kondisi awalnya salah, blok kode akan dijalankan sekali sebelum kondisi dievaluasi.

---
+++ DAY16_Perulangan_Do_while.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class perulangan_do_while {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("loop ke-" + i);
+            i++; 
+        } 
+        while (i < 221);
+
+    }
+}
