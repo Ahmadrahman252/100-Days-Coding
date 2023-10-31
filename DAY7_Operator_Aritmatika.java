1b4a803b593025bb24c9ff103877f38f3bcedfbc
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 11:31:31 +0800

    DAY7
    Operator aritmatika adalah simbol atau tanda matematika yang digunakan untuk melakukan operasi aritmatika (perhitungan matematika).

---
+++ DAY7_Operator_Aritmatika.java
@@ -0,0 +1,26 @@
+public class operator_aritmatika {
+    public static void main(String[] args) {
+        int angka1 = 18;
+        int angka2 = 29;
+
+        // Penjumlahan
+        int Penjumlahan = angka1 + angka2;
+        System.out.println("Penjumlahan: " + Penjumlahan);
+
+        // Pengurangan
+        int Pengurangan = angka1 - angka2;
+        System.out.println("Pengurangan: " + Pengurangan);
+
+        // Perkalian
+        int Perkalian = angka1 * angka2;
+        System.out.println("Perkalian: " + Perkalian);
+
+        // Pembagian
+        int Pembagian = angka1 / angka2;
+        System.out.println("Pembagian: " + Pembagian);
+
+        // Modulus
+        int sisaBagi = angka1 % angka2;
+        System.out.println("Sisa hasil bagi: " + sisaBagi);
+    }
+}
