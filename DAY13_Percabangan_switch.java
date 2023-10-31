3cfc1fa57e0634f1e5a09faf4f3de907942fcbd8
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:00:02 +0800

    DAY13
    Percabangan switch digunakan untuk membandingkan suatu pilihan dengan sejumlah nilai yang sesuai

---
+++ DAY13_Percabangan_switch.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class percabanagan_switch {
+    public static void main(String[] args) {
+        int pilihan = 2;
+        switch (pilihan) {
+            case 1:
+                System.out.println("Anda memilih opsi 1");
+                break;
+            case 2:
+                System.out.println("Anda memilih opsi 2");
+                break;
+            default:
+                System.out.println("Pilihan tidak valid");
+        }
+    }
+}
