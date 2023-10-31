c291aa825689d4d671fdb89a12ee0646574dbbeb
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:09:09 +0800

    DAY15
    Pernyataan while melakukan pengulangan (loop) membuat kode secara berulang selama suatu kondisi tertentu benar (true).

---
+++ DAY15_Perulangan_While.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class PERULANGAN__While {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 20000) {
+            System.out.println("Iterasi ke-" + i);
+            i++; 
+        }
+    }
+}
