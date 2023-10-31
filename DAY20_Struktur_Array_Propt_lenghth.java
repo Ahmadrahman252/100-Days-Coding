99749f8b0b01ad95a5b9c809dcc9137e13f69a1f
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 12:26:24 +0800

    DAY20
    Panjang (length) array Menghitung jumlah elemen atau item yang ada dalam array.

---
+++ DAY20_Struktur_Array_Propt_lenghth.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class strukturdata_array_propt_length {
+    public static void main(String[] args) {
+        int[] nilai = {1,2,3,3,4,5,6,7,8,9,9,9,9};
+        int panjang = nilai.length;
+        System.out.println("Panjang array: " + panjang);
+    }
+}
