9b69fa577bcd1dc294a771404a78e9e187298325
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-10-31 11:50:47 +0800

    DAY11
    Percabangan adalah Pengambilan keputusan berdasarkan kondisi tertentu. percabangan memungkinkan program untuk melakukan tindakan yang berbeda tergantung pada apakah suatu kondisi terpenuhi atau tidak.

---
+++ DAY11_Percabangan_If_Else.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class percabangan_if_else {
+    public static void main(String[] args) {
+        int nilai = 81;
+        if (nilai >= 90) {
+            System.out.println("Nilai : A");
+        }
+         else {
+            System.out.println("Nilai : D");
+        }
+    }
+}
