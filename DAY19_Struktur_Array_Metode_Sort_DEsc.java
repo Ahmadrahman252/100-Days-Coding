07ad43db46bec298e3dc31b288364647b23fa705
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-02 06:48:44 +0800

    DAY19
    sama saja dengaan sort hanya menggunakan reverse untuk descending nilai

--- DAY19_Struktur_Array_Metode_Sort_DEsc.java
+++ DAY19_Struktur_Array_Metode_Sort_DEsc.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Clsses/Class.java to edit this template
  */
 
 /**
