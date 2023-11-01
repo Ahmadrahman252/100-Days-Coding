06dc586129cbc50d35aa39dbf6bdfcd9b967def0
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-02 06:39:11 +0800

    Create DAY25_Struktur_Arays_Metode_BinarySearch.java
    metode binarySearch berfungsi mencari nilai dalam array yang sudah diurutkan secara terurut biner (binary search).
    Ketika hasil pencarian binarySearch lebih besar atau sama dengan nol (posisi >= 0), itu berarti elemen yang dicari ditemukan dalam array

---
+++ DAY25_Struktur_Arays_Metode_BinarySearch.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Arrays;
+public class struktur_array_binarysearch {
+    public static void main(String[] args) {
+        int[] angka = {2, 3, 4, 5, 6,7,8,12};
+
+        int nilai = 7;
+        int indeks = Arrays.binarySearch(angka, nilai);
+
+        if (indeks >= 0) {
+            System.out.println("nilai " +nilai + " berada pada indeks " + indeks);
+        } else {
+            System.out.println("Elemen " + nilai + " tidak ditemukan didalam array");
+        }
+    }
+}
+
