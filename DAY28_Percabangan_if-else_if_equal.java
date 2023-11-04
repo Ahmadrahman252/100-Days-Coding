71240ec8bf0911a6e51d0c71aa3f287534e7e9ff
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-04 10:49:23 +0800

    DAY28
    Sama sepert sebelumnya hanya saja menggunakan else if

---
+++ DAY28_Percabangan_if-else_if_equal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class percabangan_ifelse_if {
+    public static void main(String[] args) {
+        String warna = "hijau";
+        if(warna.equals("merah")){
+        System.out.println("Stop You Kissama");
+        }else if(warna.equals("kuning")){
+        System.out.println("Be Care");
+        }else if(warna.equals("hijau")){
+        System.out.println("jalan");
+        }
+    }
+}
