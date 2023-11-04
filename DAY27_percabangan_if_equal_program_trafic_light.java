260a809e7b6f6f2dee74a587fcbc5d703b1d950a
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-04 10:42:42 +0800

    DAY27
    Membuat program lampu merah menggunakan if dan equal
    equal untuk mencari yang sama

---
+++ DAY27_percabangan_if_equal_program_trafic_light.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Percabangan_if_equal_program_trafic_light {
+    public static void main(String[] args) {
+        String warna = "merah";
+        if(warna.equals("merah")){
+        System.out.println("STOP");
+        }
+
+        if(warna.equals("kuning")){
+        System.out.println("READY/BE CAREFULL");
+        }
+
+        if(warna.equals("hijau")){
+        System.out.println("WIW GO");
+        }
+    }
+}
