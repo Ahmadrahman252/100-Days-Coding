a3001e4d0f895da6857891f66fa0cb868b4d48cc
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-07 14:19:00 +0800

    DAY30
    Ukuran Penyimpanan: Tipe data shorT memiliki ukuran r 2 byte. Jadi, tipe data indigunakan untuk menghemat ruang penyimpanan

---
+++ DAY30_Typedata_Primitif_Short.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class tipedata_primitif_short {
+    public static void main(String[] args) {
+        short nilai1 = 12;
+        short nilai2 = 7;
+        
+        short hasil1 = (short)(nilai1 * nilai2);
+        short hasil2 = (short)(nilai1 + nilai2);
+        short hasil3 = (short)(nilai1 - nilai2);
+        short hasil4 = (short)(nilai1 / nilai2);
+        short hasil5 = (short)(nilai1 % nilai2);
+        System.out.println("Hasil kali: " + hasil1);
+        System.out.println("Hasil jumlah: " + hasil2);
+        System.out.println("Hasil kurang: " + hasil3);
+        System.out.println("Hasil bagi: " + hasil4);
+        System.out.println("Hasil modulus: " + hasil5);
+    }
+}
+
