33324d921957e053a0373d412e7b0f2f1681c7e2
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-08 07:32:58 +0800

    DAY31
    Tipe data int digunakan pada biilangan bulat yang lebih besar dari byte. int memiliki ukuran 32 bit.

---
+++ DAY31_Typedata_primitif_aritmatika_int.java
@@ -0,0 +1,18 @@
+
+public class typedata_primitif_aritmatika_int {
+    public static void main(String[] args) {
+        int nilai1 = 20;
+        int nilai2 = 30;
+        int hasil1 = nilai1 + nilai2;
+        int hasil2 = nilai1 * nilai2;
+        int hasil3 = nilai1 - nilai2;
+        int hasil4 = nilai1 / nilai2;
+        int hasil5 = nilai1 % nilai2;
+        
+        System.out.println("Hasil penjumlahan: " + hasil1);
+        System.out.println("Hasil kali: " + hasil2);
+        System.out.println("Hasil kurang: " + hasil3);
+        System.out.println("Hasil bagi: " + hasil4);
+        System.out.println("Hasil modulus sisa bagi: " + hasil5);
+    }
+}
