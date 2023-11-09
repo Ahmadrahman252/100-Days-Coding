5e8d2078ea01ca0c5461011ddcfa6c76ca7bab7c
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-09 12:12:41 +0800

    DAY32
    long adalah tipe data 64-bit yang digunakan pada bilangan bulat besar.

---
+++ DAY32_Typedata_primtif_long_aritmatika.java
@@ -0,0 +1,17 @@
+public class tipedata_Primitif_long {
+    public static void main(String[] args) {
+        long nilai1 = 1234567891011121314L;
+        long nilai2 = 1234567891911121314L;
+
+        long hasil1 = nilai1 +  nilai2;
+        long hasil2 = nilai1 - nilai2;
+        long hasil3 = nilai1 * nilai2;
+        long hasil4 = nilai1 / nilai2;
+        long hasil5 = nilai1 % nilai2;
+        System.out.println("Hasil penambahan: " + hasil1);
+        System.out.println("Hasil kurang: " + hasil2);
+        System.out.println("Hasil perkalian: " + hasil3);
+        System.out.println("Hasil pembagian: " + hasil4);
+         System.out.println("Hasil modulus: " + hasil5);
+    }
+}
