cf923826d3d26877f13cf54eac1be97ac0e76918
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-10 13:38:33 +0800

    DAY33
    float Menggunakan 32bit untuk bilangan pecahan atau floatig point

---
+++ DAY33_TypeData_Primitif_float._aritmatika.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class typedata_primitif_float {
+    public static void main(String[] args) {
+        float nilai1 = 12.2f;
+        float nilai2 =20.0f;
+        
+        float hasil1 = nilai1 + nilai2;
+        float hasil2 = nilai1 * nilai2;
+        float hasil3 = nilai1 - nilai2;
+        float hasil4 = nilai1 / nilai2;
+        float hasil5 = nilai1 % nilai2;
+        
+         System.out.println("Hasil tambah: " + hasil1);
+          System.out.println("Hasil kali: " + hasil2);
+           System.out.println("Hasil kurang: " + hasil3);
+            System.out.println("Hasil bagi: " + hasil4);
+             System.out.println("Hasil modulus: " + hasil5);
+    }
+}
+
