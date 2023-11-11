21fe7bdff43077c6c06a57302a60d3c748b7d959
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-12 06:30:23 +0800

    DAY35
    Mengubah typedata  char menjadi string
    char dapat menampung huruf, angka, tanda baca. tipe data char diapit oleh tanda kutip (' ').

---
+++ DAY35_Typedata_Primitif_char_charToString.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class lar {
+    public static void main(String[] args) {
+        char[] nilai = {'K','E','P','A','L','A',' ','K','U','P','O','T','O','N','G' };
+        String hasil = new String(nilai);
+        System.out.println("typedata char jadi string: " + hasil);
+    }
+}
