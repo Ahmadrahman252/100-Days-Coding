9df552db076b0e4ae282762b708f818b7fa1fc50
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-11 09:03:07 +0800

    DAY34
    double membutuhkan 8 byte (64 bit) Bedanya dengan float adalah tidak perlu menggunakan f untuk literal

---
+++ DAY34_Typedata_primitif_double_aritmatika.java
@@ -0,0 +1,19 @@
+public class typedata_primitif_doublr_aritmatika {
+    public static void main(String[] args) {
+        double nilai1 = 20.7;
+        double nilai2 = 14.1;
+
+        double hasil1 = nilai1 + nilai2;
+        double hasi21 = nilai1 * nilai2;
+        double hasil3 = nilai1 - nilai2;
+        double hasil4 = nilai1 / nilai2;
+        double hasil5 = nilai1 % nilai2;
+        
+        System.out.println("hasil jumlah:"+hasil1);
+         System.out.println("hasil Kali:"+hasi21);
+          System.out.println("hasil kurang:"+hasil3);
+           System.out.println("hasil bagi:"+hasil4);
+            System.out.println("hasil modulus:"+hasil5);
+    }
+}
+
