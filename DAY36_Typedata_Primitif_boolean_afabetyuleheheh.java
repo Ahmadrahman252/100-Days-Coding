ffef8509943d8968309315c95a272bf7b5321315
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-13 08:58:34 +0800

    DAY36
    boolean hanya berisi true or false

---
+++ DAY36_Typedata_Primitif_boolean_afabetyuleheheh.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class typedata_primitif_boolean {
+    public static void main(String[] args) {
+        boolean betul = true;
+        boolean tidakbetul = false;
+
+        System.out.println("durian apakah enak: " + betul);
+        System.out.println("hidup ini adil: " + tidakbetul);
+    }
+}
