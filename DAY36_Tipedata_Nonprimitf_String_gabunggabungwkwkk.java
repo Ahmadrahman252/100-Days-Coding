4eb78e5907d680c4e020403fb1d5051e256d3b0c
Ahmad Rahman <102941683+Ahmadrahman252@users.noreply.github.com>
2023-11-14 08:01:59 +0800

    DAY36
    Hanya contoh pengggunaan String menggunkan opartor + untuk mengabungkan denganvaraiavel

---
+++ DAY36_Tipedata_Nonprimitf_String_gabunggabungwkwkk.java
@@ -0,0 +1,11 @@
+public class tipedata_nonprimitif_String {
+    public static void main(String[] args) {
+        String aku = "Hidup Lagi Capek Capeknya";
+        String sayang = "Ahmad";
+        
+        System.out.println(aku+" malah ketemu "+ sayang);
+    }
+}
+
+
+
